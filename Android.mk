4ed3cbe229b3be634fe03856dff3fc94cf5bdc2e
Sumadhura Kalyan Singamchetti <ssingamc@qti.qualcomm.com>
2025-01-24 12:54:25 +0530

    touch-driver : enable nt36xxx in bengal and khaje
    enable nt36xxx for bengal config
    
    Change-Id: Ie9ce7b32f6d8f0191463bfc6b18e8c1e02ddfed7
    Signed-off-by: Sumadhura Kalyan Singamchetti <ssingamc@qti.qualcomm.com>

--- Android.mk
+++ Android.mk
@@ -533,6 +533,30 @@ else ifeq ($(TARGET_BOARD_PLATFORM), bengal)
        include $(DLKM_DIR)/Build_external_kernelmodule.mk
        ###########################################################
 
+
+       ###########################################################
+       include $(CLEAR_VARS)
+       LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
+       LOCAL_MODULE              := nt36xxx-i2c.ko
+       LOCAL_MODULE_KBUILD_NAME  := nt36xxx-i2c.ko
+       LOCAL_MODULE_TAGS         := optional
+       #LOCAL_MODULE_DEBUG_ENABLE := true
+       LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+       include $(DLKM_DIR)/Build_external_kernelmodule.mk
+       ###########################################################
+
+       ###########################################################
+       include $(CLEAR_VARS)
+       LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
+       LOCAL_MODULE              := qts.ko
+       LOCAL_MODULE_KBUILD_NAME  := qts.ko
+       LOCAL_MODULE_TAGS         := optional
+       #LOCAL_MODULE_DEBUG_ENABLE := true
+       LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+       include $(DLKM_DIR)/Build_external_kernelmodule.mk
+       ###########################################################
+
+
 else ifeq ($(TARGET_BOARD_PLATFORM), malabar)
 
        ###########################################################
